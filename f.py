0f0d103c41e23dc3f0bc0ae7a5c0743a72bdfa3a
Alexey <carrom@i.ua>
2024-03-13 12:58:01 +0200

    Update f.py

--- f.py
+++ f.py
@@ -1,5 +1,2 @@
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-[![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)](https://www.python.org)
